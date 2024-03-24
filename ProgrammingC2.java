357dfbc8f318e54446cb0f4b2afe61fdbb8fefa4
prafull wahatule <prafullwahtule@gmail.com>
2024-03-25 00:27:26 +0530

    first commit

---
+++ ProgrammingC2.java
@@ -0,0 +1,9 @@
+public class ProgrammingC2 {
+    public static void main(String[]args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
