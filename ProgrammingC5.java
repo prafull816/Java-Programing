d3568d9fe097b7970d9c08cdd9cd060c9bd1f3b5
prafull wahatule <prafullwahtule@gmail.com>
2024-03-26 22:51:44 +0530

    new

---
+++ ProgrammingC5.java
@@ -0,0 +1,9 @@
+/**
+ * ProgrammingC5
+ */
+public class ProgrammingC5 {
+
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****\n *****");
+    }
+}
---
+++ ProgrammingC8.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ProgrammingC8 {
+    public static void main(String[] args) {
+        System.out.println("Please Enter Your Name : ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Welcome " +  name + " To Hello World");
+
+    }
+}
